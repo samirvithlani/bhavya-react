522199b08753f37233437b261af724bfd8f4c5cf
samirvithlani <samir.vithlani83955@gmail.com>
2023-07-21 10:25:04 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import './header.css'
 function App() {
+  var divstyle ={
+    color:"red",
+    backgroundColor:"yellow"
+  }
+  //JSX expression
+  //every tag must have closing tag
+  // at time only one tag can be returned
+  var a = 10;
+  var name = "Bhavya";
+  var isActive = false;
+  var address = {
+    city: "Bangalore",
+    state: "Karnataka",
+    street: {
+      name: "1st main",
+      pincode: 560001,
+    },
+  };
+  var employees = [
+    {
+      id: 1,
+      name: "Bhavya",
+      age: 21,
+    },
+    {
+      id: 2,
+      name: "parth",
+      age: 22,
+    },
+    {
+      id: 3,
+      name: "Rahul",
+      age: 23,
+    },
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+        <h1>HEader...</h1>
+      </div>
+      <div style={divstyle}>
+          div style
+      </div>
+      <h1>HELLO.....</h1>
+      {a}
+      <h1>value of a = {a}</h1>
+      <h2>name = {name}</h2>
+      <h3>{isActive == true ? "Active" : "Not Active"}</h3>
+      <h1>{address.city}</h1>
+      <h1>{address.state}</h1>
+      <h2>{address.street.name}</h2>
+
+
+      <h1 style={{color:"red"}}>ok</h1>
+
+      {/* {
+        employees.forEach((emp)=>{
+          <h1>{emp.name}</h1>
+        })
+      }     */}
+      {employees.map((emp) => {
+        return (
+          <div>
+            <h1>{emp.id}</h1>
+            <h1>
+              {
+                emp.age> 21 ? <p style={{color:"red"}}>{emp.name}</p> : <p style={{color:"green"}}>{emp.name}</p>
+              }
+            </h1>
+            <h1>{emp.age}</h1>
+          </div>
+        );
+      })}
     </div>
   );
 }
